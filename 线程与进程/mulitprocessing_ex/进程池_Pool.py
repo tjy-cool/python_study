7d9e07aa8f55891d5e9dd9270d9f5337807fa6d2
tjy-tool <990423909@qq.com>
2017-10-08 22:17:07 +0800

    add 进程池_Pool

--- 线程与进程/mulitprocessing_ex/进程池_Pool.py
+++ 线程与进程/mulitprocessing_ex/进程池_Pool.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python
 # Funtion:      
-# Filename:     
+# Filename:
+
+from multiprocessing import  Process, Pool
+import time,os
+
+def Foo(i):
+    time.sleep(2)
+    print('in process', os.getpid())
+    return i+100
+
+def Bar(args):
+    print('--->exec done:', args, os.getpid())
+
+if __name__ == '__main__':
+    pool = Pool(processes=10)
+    print('主进程', os.getpid())
+    for i in range(10):
+        # pool.apply(func=Foo, args=(i,))
+        # pool.apply_async(func=Foo, args=(i,))
+        pool.apply_async(func=Foo, args=(i,), callback=Bar)
+
+    print('end')
+    pool.close()
+    pool.join()
